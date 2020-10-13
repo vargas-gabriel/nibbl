531b4f8881440bd13cbb1d3761e07c260a08aab1
Gabriel Vargas <vargasgabriel@protonmail.com>
2020-10-13 17:06:49 -0500

    customized nav bar

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,49 +1,55 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
-import LogOutButton from '../LogOutButton/LogOutButton';
-import './Nav.css';
-import mapStoreToProps from '../../redux/mapStoreToProps';
+import React from "react";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import LogOutButton from "../LogOutButton/LogOutButton";
+import "./Nav.css";
+import mapStoreToProps from "../../redux/mapStoreToProps";
 
 const Nav = (props) => {
-  let loginLinkData = {
-    path: '/login',
-    text: 'Login / Register',
-  };
+	let loginLinkData = {
+		path: "/login",
+		text: "Login / Register",
+	};
 
-  if (props.store.user.id != null) {
-    loginLinkData.path = '/user';
-    loginLinkData.text = 'Home';
-  }
+	if (props.store.user.id != null) {
+		loginLinkData.path = "/user";
+		loginLinkData.text = "Home";
+	}
 
-  return (
-    <div className="nav">
-      <Link to="/home">
-        <h2 className="nav-title">Prime Solo Project</h2>
-      </Link>
-      <div className="nav-right">
-        <Link className="nav-link" to={loginLinkData.path}>
-          {/* Show this link if they are logged in or not,
+	return (
+		<div className='nav'>
+			<Link to='/home'>
+				<h2 className='nav-title'>nibbl</h2>
+			</Link>
+			<div className='nav-right'>
+				<Link className='nav-link' to={loginLinkData.path}>
+					{/* Show this link if they are logged in or not,
           but call this link 'Home' if they are logged in,
           and call this link 'Login / Register' if they are not */}
-          {loginLinkData.text}
-        </Link>
-        {/* Show the link to the info page and the logout button if the user is logged in */}
-        {props.store.user.id && (
-          <>
-            <Link className="nav-link" to="/info">
-              Info Page
-            </Link>
-            <LogOutButton className="nav-link" />
-          </>
-        )}
-        {/* Always show this link since the about page is not protected */}
-        <Link className="nav-link" to="/about">
-          About
-        </Link>
-      </div>
-    </div>
-  );
+					{loginLinkData.text}
+				</Link>
+				{/* Show the link to the info page and the logout button if the user is logged in */}
+				{props.store.user.id && (
+					<>
+						{/* <Link className='nav-link' to='/info'>
+							Home
+						</Link> */}
+						<LogOutButton className='nav-link' />
+					</>
+				)}
+				{/* Always show this link since the about page is not protected */}
+				<Link className='nav-link' to='/about'>
+					About
+				</Link>
+				<Link className='nav-link' to='/about'>
+					Create
+				</Link>
+				<Link className='nav-link' to='/about'>
+					Saved
+				</Link>
+			</div>
+		</div>
+	);
 };
 
 export default connect(mapStoreToProps)(Nav);

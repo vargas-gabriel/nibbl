e2c725b785ddc4c743d7bbdaabaf0439a853b9de
Gabriel Vargas <vargasgabriel@protonmail.com>
2020-10-13 16:29:39 -0500

    set up meal info page, changed user and info pages

--- src/components/InfoPage/InfoPage.js
+++ src/components/InfoPage/InfoPage.js
@@ -1,29 +1,35 @@
-import React from 'react';
+import React from "react";
+import { connect } from "react-redux";
+// import LogOutButton from "../LogOutButton/LogOutButton";
+import mapStoreToProps from "../../redux/mapStoreToProps";
 
 // This is one of our simplest components
 // It doesn't have local state, so it can be a function component.
 // It doesn't dispatch any redux actions or display any part of redux state
 // or even care what the redux state is, so it doesn't need 'connect()'
 
-const InfoPage = () => (
-  <div>
-    <p>Info Page</p>
-  </div>
-);
+// const InfoPage = () => (
+//   <div>
+//     <p>Info Page</p>
+//   </div>
+// );
 
 // If you needed to add local state or other things,
 // you can make it a class component like:
 
-/*
 class InfoPage extends React.Component {
-
-  render() {
-    return (
-      <div>
-        <p>Info Page</p>
-      </div>
-    )
-  }
+	render() {
+		return (
+			<div>
+				<h1 id='welcome'>
+					Here are your results, {this.props.store.user.username}
+				</h1>
+				<div>Breakfast</div>
+				<div>Lunch</div>
+				<div>Dinner</div>
+			</div>
+		);
+	}
 }
-*/
-export default InfoPage;
+
+export default connect(mapStoreToProps)(InfoPage);
--- src/components/UserPage/UserPage.js
+++ src/components/UserPage/UserPage.js
@@ -1,19 +1,24 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import LogOutButton from '../LogOutButton/LogOutButton';
-import mapStoreToProps from '../../redux/mapStoreToProps';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+// import LogOutButton from "../LogOutButton/LogOutButton";
+import mapStoreToProps from "../../redux/mapStoreToProps";
 
 class UserPage extends Component {
-  // this component doesn't do much to start, just renders some user info to the DOM
-  render() {
-    return (
-      <div>
-        <h1 id="welcome">Welcome, {this.props.store.user.username}!</h1>
-        <p>Your ID is: {this.props.store.user.id}</p>
-        <LogOutButton className="log-in" />
-      </div>
-    );
-  }
+	// this component doesn't do much to start, just renders some user info to the DOM
+	render() {
+		return (
+			<div>
+				<h1 id='welcome'>Welcome, {this.props.store.user.username}!</h1>
+				{/* <p>Your ID is: {this.props.store.user.id}</p> */}
+				{/* <LogOutButton className="log-in" /> */}
+				<p>To begin, select a diet from the list:</p>
+				<div>Ketogenic</div>
+				<div>Paleo</div>
+				<div>Vegan</div>
+				<div>Vegetarian</div>
+			</div>
+		);
+	}
 }
 
 // this allows us to use <App /> in index.js

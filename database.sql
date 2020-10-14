db8dd3954f7ce37aa6a6f820843d102d7a2c43ac
Gabriel Vargas <vargasgabriel@protonmail.com>
2020-10-14 13:47:11 -0500

    created diets table and populated it with diets

--- database.sql
+++ database.sql
@@ -8,3 +8,15 @@ CREATE TABLE "user" (
     "username" VARCHAR (80) UNIQUE NOT NULL,
     "password" VARCHAR (1000) NOT NULL
 );
+CREATE TABLE "user_meals" (
+    "id" SERIAL PRIMARY KEY,
+    "USER_ID" INT,
+    "MEAL_ID" INT
+);
+CREATE TABLE "diets" (
+    "id" SERIAL PRIMARY KEY,
+    "name" VARCHAR (80) NOT NULL
+    
+);
+INSERT INTO "diets" ("name")
+VALUES ('Ketogenic'), ('Vegan'), ('Vegetarian'), ('Paleo');

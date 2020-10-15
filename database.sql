9134f7abdc2be5a19a7877b2bf39d36445b62335
Gabriel Vargas <vargasgabriel@protonmail.com>
2020-10-14 21:52:23 -0500

    populated database with faux data

--- database.sql
+++ database.sql
@@ -31,11 +31,22 @@ CREATE TABLE "meals" (
     "id" SERIAL PRIMARY KEY,
     "mealName" VARCHAR (80) NOT NULL,
     "calories" VARCHAR (1000) NOT NULL,
-    "youtube" VARCHAR (2000) NOT NULL
+    "youtube" VARCHAR (2000) NOT NULL,
+    "type" INT NOT NULL,
+    "time" INT NOT NULL
 );
 --juction for meals and diets tables
 CREATE TABLE "meals_diets" (
 	"id" SERIAL PRIMARY KEY,
     "DIET_ID" INT REFERENCES "diets",
     "MEAL_ID" INT REFERENCES "meals"
+    "TIME_ID" INT REFERENCES "meals"
 );
+
+INSERT INTO "meals" ("mealName", "calories", "youtube", "type", "time")
+VALUES ('Keto Breakfast Casserole', '500', 'https://www.youtube.com/watch?v=1Aydu2xmue8&ab_channel=LowCarbRecipeswithJennifer', 1, 1;
+
+INSERT INTO "meals" ("mealName", "calories", "youtube", "type", "time")
+VALUES ('Keto Lunch', '600', 'youtube.com', 1, 2), ('Keto Dinner', '800', 'youtube', 1, 3), ('Vegan Breakfast', '500', 'youtube', 2, 1), ('Vegan Lunch', '430', 'youtube', 2, 2), 
+('Vegan Dinner', '650', 'youtube', 2, 3), ('Vegetarian Breakfast', '690', 'youtube', 3, 1), ('Vegetarian Lunch', '680', 'youtube', 3, 2), ('Vegetarian Dinner', '750', 'youtube', 3, 3),
+('Paleo BreakFast', '550', 'youtube', 4, 1), ('Paleo Lunch', '575', 'youtube', 4, 2), ('Paleo Dinner', '550', 'youtube', 4, 3);
--- src/components/CreateMealPage/CreateMealPage.js
+++ src/components/CreateMealPage/CreateMealPage.js
@@ -8,7 +8,8 @@ class CreateMealPage extends Component {
 		meal: "",
 		calories: "",
 		youtube: "",
-		recipe: "",
+		type: "",
+		time: "",
 	};
 	handleChange = (event, propertyName) => {
 		this.setState({
@@ -25,8 +26,9 @@ class CreateMealPage extends Component {
 			meal: "",
 			calories: "",
 			youtube: "",
-			recipe: "",
+			type: "",
 		});
+		alert("meal saved! :)");
 	};
 
 	render() {
@@ -35,25 +37,46 @@ class CreateMealPage extends Component {
 				<p> Create Your Own Meal</p>
 				<form onSubmit={this.handleSubmit}>
 					<input
+						required
 						value={this.state.meal}
 						type='text'
 						placeholder='Meal Name'
 						onChange={(event) => this.handleChange(event, "meal")}></input>
 					<input
+						required
 						value={this.state.calories}
-						type='text'
+						type='number'
 						placeholder='Calories'
 						onChange={(event) => this.handleChange(event, "calories")}></input>
 					<input
+						required
 						value={this.state.youtube}
 						type='text'
 						placeholder='Youtube Link'
 						onChange={(event) => this.handleChange(event, "youtube")}></input>
-					<input
-						value={this.state.recipe}
-						type='text'
-						placeholder='Recipe URL'
-						onChange={(event) => this.handleChange(event, "recipe")}></input>
+					<select
+						required
+						value={this.state.type}
+						onChange={(event) => this.handleChange(event, "type")}>
+						<option disabled value='0'>
+							Pick Diet
+						</option>
+						<option value={1}>Keto</option>
+						<option value={2}>Vegan</option>
+						<option value={3}>Vegetarian</option>
+						<option value={4}>Paleo</option>
+					</select>
+					<select
+						required
+						value={this.state.time}
+						onChange={(event) => this.handleChange(event, "time")}>
+						<option disabled value='0'>
+							Pick Type
+						</option>
+						<option value={1}>Breakfast</option>
+						<option value={2}>Lunch</option>
+						<option value={3}>Dinner</option>
+					</select>
 					<button type='submit'>Save Meal</button>
 				</form>
 			</div>

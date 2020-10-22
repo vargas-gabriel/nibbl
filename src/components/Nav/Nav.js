0e8500c671e0b06061a357b38e1439573d475c64
Gabriel Vargas <vargasgabriel@protonmail.com>
2020-10-22 17:12:44 -0500

    conditionally rendered nav bar so only visible if logged in, and changed text color of login form

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -17,38 +17,49 @@ const Nav = (props) => {
 	}
 
 	return (
-		<div className='nav'>
-			<Link to='/home'>
-				<h2 className='nav-title'>nibbl</h2>
-			</Link>
-			<div className='nav-right'>
-				<Link className='nav-link' to={loginLinkData.path}>
-					{/* Show this link if they are logged in or not,
-          but call this link 'Home' if they are logged in,
-          and call this link 'Login / Register' if they are not */}
-					{loginLinkData.text}
-				</Link>
-				{/* Show the link to the info page and the logout button if the user is logged in */}
-				{props.store.user.id && (
-					<>
-						{/* <Link className='nav-link' to='/info'>
+		<div>
+			{/* Show the link to the info page and the logout button if the user is logged in */}
+			{props.store.user.id && (
+				<>
+					{/* <Link className='nav-link' to='/info'>
 							Home
 						</Link> */}
-						<LogOutButton className='nav-link' />
-					</>
-				)}
-				{/* Always show this link since the about page is not protected */}
-				{/* <Link className='nav-link' to='/about'>
+
+					<div className='nav'>
+						<Link to='/home'>
+							<h2 className='nav-title'>nibbl</h2>
+						</Link>
+						<div className='nav-right'>
+							<Link className='nav-link' to={loginLinkData.path}>
+								{/* Show this link if they are logged in or not,
+          but call this link 'Home' if they are logged in,
+          and call this link 'Login / Register' if they are not */}
+								{loginLinkData.text}
+							</Link>
+							{/* Show the link to the info page and the logout button if the user is logged in */}
+							{props.store.user.id && (
+								<>
+									{/* <Link className='nav-link' to='/info'>
+							Home
+						</Link> */}
+									<LogOutButton className='nav-link' />
+								</>
+							)}
+							{/* Always show this link since the about page is not protected */}
+							{/* <Link className='nav-link' to='/about'>
 					About
 				</Link> */}
-				<Link className='nav-link' to='/create'>
-					Create
-				</Link>
+							<Link className='nav-link' to='/create'>
+								Create
+							</Link>
 
-				<Link className='nav-link' to='/saved'>
-					My Profile
-				</Link>
-			</div>
+							<Link className='nav-link' to='/saved'>
+								My Profile
+							</Link>
+						</div>
+					</div>
+				</>
+			)}
 		</div>
 	);
 };
